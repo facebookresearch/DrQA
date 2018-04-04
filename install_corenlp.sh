541f702bde4eb20db5ed00ed27c8a05ac4919521
JP <jp@jpatrickpark.com>
2018-04-04 13:02:17 -0400

    fix install_corenlp.sh so that it will run on prince; also add some files to sbatch

--- install_corenlp.sh
+++ install_corenlp.sh
@@ -13,15 +13,15 @@ DOWNLOAD_PATH="${path:-data/corenlp}"
 echo "Will download to: $DOWNLOAD_PATH"
 
 # Download zip, unzip
-pushd "/tmp"
+#pushd "/tmp"
 wget -O "stanford-corenlp-full-2017-06-09.zip" "http://nlp.stanford.edu/software/stanford-corenlp-full-2017-06-09.zip"
 unzip "stanford-corenlp-full-2017-06-09.zip"
-rm "stanford-corenlp-full-2017-06-09.zip"
-popd
+#rm "stanford-corenlp-full-2017-06-09.zip"
+#popd
 
 # Put jars in DOWNLOAD_PATH
 mkdir -p "$DOWNLOAD_PATH"
-mv "/tmp/stanford-corenlp-full-2017-06-09/"*".jar" "$DOWNLOAD_PATH/"
+mv "stanford-corenlp-full-2017-06-09/"*".jar" "$DOWNLOAD_PATH/"
 
 # Append to bashrc, instructions
 while read -p "Add to ~/.bashrc CLASSPATH (recommended)? [yes/no]: " choice; do
---
+++ interactive.s
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2:00:00
+#SBATCH --mem=64GB
+#SBATCH --job-name=myDrQA
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jp.park@nyu.edu
+#SBATCH --output=output/slurm_%j.out
+#SBATCH --gres=gpu:2
+  
+module purge
+module load cuda/9.0.176
+module load cudnn/9.0v7.0.5 
+#module load pytorch/python3.6/0.3.0_4
+#module load python3/intel/3.6.3  
+module load gcc/6.3.0
+RUNDIR=$SCRATCH/my_project/run-${SLURM_JOB_ID/.*}
+mkdir -p $RUNDIR
+SRCDIR=$SCRATCH/DrQA/scripts/pipeline/
+cd $RUNDIR
+export CLASSPATH=$CLASSPATH:$SCRATCH/data/corenlp/*
+$HOME/anaconda3/bin/python $SRCDIR/interactive.py < $SCRATCH/input.txt 
+
+
